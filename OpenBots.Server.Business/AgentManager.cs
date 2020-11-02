04631e10637551cede376adaa1d4c1446c335482
openbots-an <an@openbots.ai>
2020-11-01 20:57:31 -0500

    Moved Repo from Previous Project

---
+++ OpenBots.Server.Business/AgentManager.cs
@@ -0,0 +1,50 @@
+﻿using OpenBots.Server.DataAccess.Repositories;
+using OpenBots.Server.Model;
+using OpenBots.Server.ViewModel;
+using System;
+using System.Linq;
+
+namespace OpenBots.Server.Business
+{
+    public class AgentManager : BaseManager, IAgentManager
+    {
+        private readonly IAgentRepository agentRepo;
+        private readonly IScheduleRepository scheduleRepo;
+        private readonly IJobRepository jobRepo;
+        private readonly IAspNetUsersRepository usersRepo;
+        private readonly ICredentialRepository credentialRepo;
+
+        public AgentManager(IAgentRepository agentRepository, IScheduleRepository scheduleRepository, IJobRepository jobRepository, IAspNetUsersRepository usersRepository,
+            ICredentialRepository credentialRepository)
+        {
+            this.agentRepo = agentRepository;
+            this.scheduleRepo = scheduleRepository;
+            this.jobRepo = jobRepository;
+            this.usersRepo = usersRepository;
+            this.credentialRepo = credentialRepository;
+        }
+
+        public AgentViewModel GetAgentDetails(AgentViewModel agentView)
+        {
+            agentView.UserName = usersRepo.Find(null, u => u.Name == agentView.Name).Items?.FirstOrDefault()?.UserName;
+            agentView.CredentialName = credentialRepo.GetOne(agentView.CredentialId??Guid.Empty)?.Name;
+
+            return agentView;
+        }
+
+        public bool CheckReferentialIntegrity(string id)
+        {
+            Guid agentId = new Guid(id);
+
+            var scheduleWithAgent = scheduleRepo.Find(0, 1).Items?
+              .Where(s => s.AgentId == agentId);
+
+            var jobWithAgent = jobRepo.Find(0, 1).Items?
+              .Where(j => j.AgentId == agentId && j.JobStatus == JobStatusType.Assigned 
+              | j.JobStatus == JobStatusType.New
+              | j.JobStatus == JobStatusType.InProgress);
+
+            return scheduleWithAgent.Count() > 0 || jobWithAgent.Count() > 0 ? true : false;
+        }
+    }
+}
---
+++ OpenBots.Server.DataAccess/Migrations/20201022165429_UpdateEmailLogs2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OpenBots.Server.DataAccess.Migrations
+{
+    public partial class UpdateEmailLogs2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SenderName",
+                table: "EmailLogs",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SenderName",
+                table: "EmailLogs");
+        }
+    }
+}
