770004a47e497d91e75d69e9f48abc6ddafe4971
adnan <adnan.nazir@accelirate.com>
2020-12-03 19:14:15 +0500

    removed url

--- OpenBots.Server.Web/ClientApp/src/environments/environment.ts
+++ OpenBots.Server.Web/ClientApp/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'https://dev.server.openbots.io/api/v1',
+  // apiUrl: 'https://dev.server.openbots.io/api/v1',
   production: true,
   // apiUrl: '/api/v1',
   healthUrl: '/healthcheck-ui',
