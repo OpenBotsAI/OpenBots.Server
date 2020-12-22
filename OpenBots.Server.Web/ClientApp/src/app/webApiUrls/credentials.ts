1bc2ff7921311537fef4eab5bb1cfd2ab7ecdd46
Muhammad Usman Khan <usman.khan@accelirate.com>
2020-12-22 20:02:45 +0500

    add enum of assset and agent modules

--- OpenBots.Server.Web/ClientApp/src/app/webApiUrls/credentials.ts
+++ OpenBots.Server.Web/ClientApp/src/app/webApiUrls/credentials.ts
@@ -1,3 +1,4 @@
 export enum CredentialsApiUrl {
   credentials = 'credentials',
+  CredentialsGetLookup = 'Credentials/GetLookup',
 }
--- OpenBots.Server.Web/ClientApp/src/app/webApiUrls/index.ts
+++ OpenBots.Server.Web/ClientApp/src/app/webApiUrls/index.ts
@@ -2,3 +2,5 @@ export * from './automations';
 export * from './automationLogs';
 export * from './ipFencing';
 export * from './credentials';
+export * from './agentsUrl';
+export * from './assetsUrl';
