97110d2a65c47acde8af7f979788c0d3caf56c19
Nicole Carrero <nicole.carrero@accelirate.com>
2021-01-08 21:43:52 +0000

    Merged PR 1360: Complete Work Item 3421
    Create property in asset entity to store SizeInBytes of all asset value types
    
    Related work items: #3421

---
+++ OpenBots.Server.DataAccess/Migrations/20210108160742_AddAssetPropertySizeInBytes.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OpenBots.Server.DataAccess.Migrations
+{
+    public partial class AddAssetPropertySizeInBytes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "SizeInBytes",
+                table: "Assets",
+                type: "bigint",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SizeInBytes",
+                table: "Assets");
+        }
+    }
+}
