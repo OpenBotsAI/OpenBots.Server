b20bdc856561cb4b8b47817b9aa9a8b92f08ae81
Nicole Carrero <nicole.carrero@OpenBots.AI>
2021-02-23 18:43:50 +0000

    Merged PR 18: Add Id Property to Queue Item Attachment View API
    Related work items: #453

--- OpenBots.Server.ViewModel/Queue/AllQueueItemAttachmentsViewModel.cs
+++ OpenBots.Server.ViewModel/Queue/AllQueueItemAttachmentsViewModel.cs
@@ -4,6 +4,7 @@ namespace OpenBots.Server.ViewModel.Queue
 {
     public class AllQueueItemAttachmentsViewModel
     {
+        public Guid Id { get; set; }
         public Guid QueueItemId { get; set; }
         public Guid FileId { get; set; }
         public long SizeInBytes { get; set; }
