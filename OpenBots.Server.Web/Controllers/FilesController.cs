e25e683f5cefb18125429454dbe06ed8e629e825
Dairon Hernandez <75703146+openbots-dh@users.noreply.github.com>
2021-03-26 16:40:38 -0400

    Fixed build error

--- OpenBots.Server.Web/Controllers/FilesController.cs
+++ OpenBots.Server.Web/Controllers/FilesController.cs
@@ -104,16 +104,16 @@ namespace OpenBots.Server.Web.Controllers
         }
 
         /// <summary>
-        /// Gets count of server files in database
+        /// Gets count of storage files in database
         /// </summary>
         /// <param name="driveName"></param>
         /// <param name="filter"></param>
-        /// <response code="200">Ok, count of server files</response>
+        /// <response code="200">Ok, count of storage files</response>
         /// <response code="400">Bad request</response>
         /// <response code="403">Forbidden</response>
         /// <response code="404">Not found</response>
         /// <response code="422">Unprocessable entity</response>
-        /// <returns>Count of all server files</returns>
+        /// <returns>Count of all storage files</returns>
         [HttpGet("count")]
         [Produces("application/json")]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -136,16 +136,16 @@ namespace OpenBots.Server.Web.Controllers
         }
 
         /// <summary>
-        /// Gets count of server folders in database
+        /// Gets count of storage folders in database
         /// </summary>
         /// <param name="driveName"></param>
         /// <param name="filter"></param>
-        /// <response code="200">Ok, count of server folders</response>
+        /// <response code="200">Ok, count of storage folders</response>
         /// <response code="400">Bad request</response>
         /// <response code="403">Forbidden</response>
         /// <response code="404">Not found</response>
         /// <response code="422">Unprocessable entity</response>
-        /// <returns>Count of all server folders</returns>
+        /// <returns>Count of all storage folders</returns>
         [HttpGet("count/folder")]
         [Produces("application/json")]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -209,16 +209,16 @@ namespace OpenBots.Server.Web.Controllers
         }
 
         /// <summary>
-        /// Provides server drive details for local storage
+        /// Provides storage drive details for local storage
         /// </summary>
         /// <param name="driveName"></param>
-        /// <response code="200">Ok, if the server drive exists</response>
+        /// <response code="200">Ok, if the storage drive exists</response>
         /// <response code="304">Not modified</response>
-        /// <response code="400">Bad request, if server drive entity is not in proper format</response>
+        /// <response code="400">Bad request, if storage drive entity is not in proper format</response>
         /// <response code="403">Forbidden</response>
-        /// <response code="404">Not found, when no server drive exists</response>
+        /// <response code="404">Not found, when no storage drive exists</response>
         /// <response code="422">Unprocessable entity</response>
-        /// <returns>Server drive details</returns>
+        /// <returns>Storage drive details</returns>
         [HttpGet("drive", Name = "GetDrive")]
         [ProducesResponseType(typeof(StorageDrive), StatusCodes.Status200OK)]
         [Produces("application/json")]
@@ -242,7 +242,7 @@ namespace OpenBots.Server.Web.Controllers
         }
 
         /// <summary>
-        /// Uploads new file/folder in server drive
+        /// Uploads new file/folder in storage drive
         /// </summary>
         /// <param name="request"></param>
         /// <response code="200">Ok, new file/folder entity created and returned</response>
@@ -480,15 +480,15 @@ namespace OpenBots.Server.Web.Controllers
         }
 
         /// <summary>
-        /// Creates new server drive
+        /// Creates new storage drive
         /// </summary>
         /// <param name="driveName"></param>
-        /// <response code="200">Ok, new server drive entity created and returned</response>
-        /// <response code="400">Bad request, when the server drive values are not in proper format</response>
+        /// <response code="200">Ok, new storage drive entity created and returned</response>
+        /// <response code="400">Bad request, when the storage drive values are not in proper format</response>
         /// <response code="403">Forbidden, unauthorized access</response>
         /// <response code="409">Conflict, concurrency error</response> 
         /// <response code="422">Unprocessable Entity, when a duplicate record is being entered</response>
-        /// <returns>Newly created server drive details</returns>
+        /// <returns>Newly created storage drive details</returns>
         [HttpPost("drive")]
         [ProducesResponseType(typeof(StorageDrive), StatusCodes.Status200OK)]
         [Produces("application/json")]
@@ -511,15 +511,15 @@ namespace OpenBots.Server.Web.Controllers
         }
 
         /// <summary>
-        /// Provides server drive names
+        /// Provides storage drive names
         /// </summary>
-        /// <response code="200">Ok, if the server drive names exist</response>
+        /// <response code="200">Ok, if the storage drive names exist</response>
         /// <response code="304">Not modified</response>
-        /// <response code="400">Bad request, if server drive name is not in proper format</response>
+        /// <response code="400">Bad request, if storage drive name is not in proper format</response>
         /// <response code="403">Forbidden</response>
-        /// <response code="404">Not found, when no server drive names exist</response>
+        /// <response code="404">Not found, when no storage drive names exist</response>
         /// <response code="422">Unprocessable entity</response>
-        /// <returns>Server drive names</returns>
+        /// <returns>Storage drive names</returns>
         [HttpGet("driveNames", Name = "GetDriveNames")]
         [ProducesResponseType(typeof(IActionResult), StatusCodes.Status200OK)]
         [Produces("application/json")]
@@ -545,6 +545,6 @@ namespace OpenBots.Server.Web.Controllers
 }
 
 //TODO additional api calls:
-//update server drive details/rename?
-//delete server drive?
+//update storage drive details/rename?
+//delete storage drive?
 //get file attributes for a file?
