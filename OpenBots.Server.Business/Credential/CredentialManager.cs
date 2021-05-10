9ee8a70c13dc483e629727260e1c74e7b4e67579
Dairon Hernandez <75703146+openbots-dh@users.noreply.github.com>
2021-05-10 14:55:26 -0400

    Add null check to password for add credential

--- OpenBots.Server.Business/Credential/CredentialManager.cs
+++ OpenBots.Server.Business/Credential/CredentialManager.cs
@@ -71,7 +71,7 @@ namespace OpenBots.Server.Business
 
         public Credential CreateGlobalCredential(GlobalCredentialViewModel request)
         {
-            if (request.PasswordSecret != null)
+            if (String.IsNullOrEmpty(request.PasswordSecret))
             {
                 //get encryption key
                 var orgId = _organizationManager.GetDefaultOrganization().Id;
@@ -106,7 +106,7 @@ namespace OpenBots.Server.Business
 
         public Credential CreateAgentCredential(AgentCredentialViewModel request)
         {
-            if (request.PasswordSecret != null)
+            if (String.IsNullOrEmpty(request.PasswordSecret))
             {
                 var encryptionKey = GetEncryptionKey();
 
